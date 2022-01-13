c149aee6e4607315601e9b0e0f99a1b1c400d07e
rakeshs <rakeshs@sfu.ca>
2022-01-13 01:33:11 +0000

    OCRTOC dataloader: use relative coord frame (wrt first view)
    + smaller dataset

--- utils/dataloader_ocrtoc.py
+++ utils/dataloader_ocrtoc.py
@@ -35,7 +35,8 @@ class DataFetcher(threading.Thread):
                 'triplet_id': triplet_id
             }
             for sample in split_data
-            for triplet_id, images in enumerate(sample['images'])
+            for triplet_id, images in enumerate(sample['images'][:50])
+            if sample['category'].lower() in ['sofa', 'chair'] # , 'cabinet/shelf/desk']
         ]
 
         self.index = 0
@@ -72,6 +73,9 @@ class DataFetcher(threading.Thread):
 
         imgs = np.zeros((3, 224, 224, 3))
         poses = np.zeros((3, 16))
+        T_world_cam0 = camera_meta_data[int(sample['images'][0])].reshape((4, 4))
+        T_cam0_world = np.linalg.inv(T_world_cam0)
+
         for idx, view in enumerate(sample['images']):
             img = cv2.imread(
                 os.path.join(img_path, f'segmented_color_{view}.png'),
@@ -81,8 +85,60 @@ class DataFetcher(threading.Thread):
             img = cv2.resize(img, (224, 224))
             img_inp = img.astype('float32') / 255.0
             imgs[idx] = img_inp[:, :, :3]
-            poses[idx] = camera_meta_data[int(view)]
-        return imgs, label, poses, pkl_item, mesh
+            T_world_cam = camera_meta_data[int(view)].reshape((4, 4))
+            T_cam0_cam = T_cam0_world @ T_world_cam
+            poses[idx] = T_cam0_cam.reshape((-1))
+
+        label_local = self.transform_label(label, poses[0])
+        # self.debug(imgs, label, label_local, pkl_item)
+
+        return imgs, label_local, poses, pkl_item, mesh
+
+    @staticmethod
+    def debug(imgs, label_world, label_local, data_id):
+        X = label_local[:, 0]
+        Y = label_local[:, 1]
+        Z = label_local[:, 2]
+
+        h = (193.3635 * np.divide(-Y, -Z)) + 112.0
+        w = (193.3635 * np.divide(X, -Z)) + 112.0
+
+        h = np.clip(h, 0, 223)
+        w = np.clip(w, 0, 223)
+
+        debug_img = imgs[0, ...]
+        indices = (
+            h.astype(np.uint16), w.astype(np.uint16),
+            np.ones((h.shape[0], ), dtype=np.uint16) * 2
+        )
+        debug_img[indices] = 1.0
+        debug_img = (debug_img * 255).astype(np.uint8)
+        cv2.imwrite(f'{data_id}_debug_img.png', debug_img)
+
+        with open(f'{data_id}_world.dat', 'wb') as f:
+            pickle.dump(label_world, f)
+
+        with open(f'{data_id}_local.dat', 'wb') as f:
+            pickle.dump(label_local, f)
+
+    @staticmethod
+    def transform_label(label, T_world_cam):
+        T_world_cam = T_world_cam.reshape((4, 4))
+        T_cam_world = np.linalg.inv(T_world_cam)
+        points_world = label[:, :3]
+        normals_world = label[:, 3:]
+
+        points_cam = T_cam_world @ DataFetcher.to_homogeneous(points_world).T
+        points_cam = (points_cam.T)[:, :3]
+        normals_cam = (T_cam_world[:3, :3] @ normals_world.T).T
+
+        return np.concatenate(
+            (points_cam, normals_cam), axis=-1
+        )
+
+    @staticmethod
+    def to_homogeneous(x):
+        return np.concatenate((x, np.ones_like(x[:, 0:1])), axis=-1)
 
     def run(self):
         while self.index < 9000000 and not self.stopped:
